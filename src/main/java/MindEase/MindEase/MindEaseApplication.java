21dd4f62f02e0f030d28dcc7367c8d385ad45992
Connor <brockcc1@cardiff.ac.uk>
2023-12-22 00:25:07 +0000

    Initial Spring project

---
+++ src/main/java/MindEase/MindEase/MindEaseApplication.java
@@ -0,0 +1,13 @@
+package MindEase.MindEase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindEaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MindEase/MindEase/MindEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package MindEase.MindEase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
